95bba10e626ed26e3b99dbf12897539aeecf1cad
Sourab Hossain <sourabhossain.fci@gmail.com>
2021-11-06 00:39:16 +0600

    DEV: #02 - Error handler

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
+// external imports
 const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const path = require("path");
 const cookieParser = require("cookie-parser");
+
+// internal imports
+const {
+	notFoundHandler,
+	errorHandler,
+} = require("./middlewares/common/errorHandler");
 
 const app = express();
 dotenv.config();
@@ -11,7 +18,7 @@ dotenv.config();
 mongoose
 	.connect(process.env.DB_CONNECTION, {
 		useNewUrlParser: true,
-		useUnifiedTopology: true
+		useUnifiedTopology: true,
 	})
 	.then(() => console.log("Database connected"))
 	.catch((errors) => console.log(errors));
@@ -31,8 +38,12 @@ app.use(cookieParser(process.env.COOKIE_SECRET));
 
 // routing setup
 
-// error handling
+// not found handler
+app.use(notFoundHandler);
+
+// common error handler
+app.use(errorHandler);
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server started on port ${process.env.PORT}`);
+	console.log(`Server started on port ${process.env.PORT}`);
 });
